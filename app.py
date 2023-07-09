71fec2e3d6017735618c97a76cd418558d07744a
George Karamanis <gkaramanis1@outlook.com>
2023-07-09 19:06:27 -0400

    changed workflow

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def index():
     # return render_template('stats.html', title='Stats')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
     
